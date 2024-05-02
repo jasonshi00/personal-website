161bc99724ab96670b7b6d696a21dc9addb90c91
shijas <shijas@umich.edu>
2024-05-02 02:31:22 -0400

    basic route with html

--- index.js
+++ index.js
@@ -3,13 +3,9 @@ const app = express();
 const port = 8000;
 
 app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
-app.get("/hello", (req, res) => {
-  res.send("Hello World!");
+  res.sendFile(__dirname + "/views/index.html");
 });
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
-
